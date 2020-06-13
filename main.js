5a9a92bf69296c78492485ffce1aa1a16d8fcd97
Connor Ocampo <connorocampo@gmail.com>
2020-06-13 00:37:12 -0400

    fix scripts

--- main.js
+++ main.js
@@ -23,3 +23,12 @@ window.onclick = function (toggle) {
     }
   }
 };
+
+// ADD ACTIVE CLASS TO NAV LINK WHEN SELECTED
+
+function addActive(e) {
+  if (document.querySelector("#nav a.active") !== null) {
+    document.querySelector("#nav a.active").classList.remove("active");
+  }
+  e.target.className = "active";
+}
--- nav.js
+++
@@ -1,8 +0,0 @@
-// ADD ACTIVE CLASS TO NAV LINK WHEN SELECTED
-
-function addActive(e) {
-  if (document.querySelector("#nav a.active") !== null) {
-    document.querySelector("#nav a.active").classList.remove("active");
-  }
-  e.target.className = "active";
-}
